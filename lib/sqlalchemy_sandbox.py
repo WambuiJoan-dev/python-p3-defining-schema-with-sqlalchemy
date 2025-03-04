6e32f4842fd203fdbe6bea9e861d8f844a3f086f
+ <+>
2025-03-04 15:47:53 +0300

    commit

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+
 
 Base = declarative_base()
 
